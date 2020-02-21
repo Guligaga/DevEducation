6859267245ad28a5acc3c12f80af93b8abfc578e
Evgeniy Koval <glggboko@gmail.com>
2020-02-21 12:10:46 +0200

    Add delStart and delEnd methods

--- src/aList/aList.js
+++ src/aList/aList.js
@@ -12,16 +12,41 @@ function Alist(arr) {
 
     this.addEnd = function addEnd(value) {
 
-        let arrOut = arr;
-        
+        let arrOut = [];
+        for (let i = 0; i < arr.length; i++)
+        {
+            arrOut[i] = arr[i]
+        }
         arrOut[arr.length] = value;
         
         return arrOut;
     }
 
+    this.delStart = function delStart() {
+
+        let arrFirst = arr[0]
+        
+        for (let i = 0; i < (arr.length - 1); i++) {
+            
+            arr[i] = arr[i + 1]
+            
+        }
+        arr.length = arr.length - 1; 
+        return arrFirst;
+    }
+
+    this.delEnd = function delEnd() {
+        
+        let arrLast = arr[arr.length - 1]
+
+        arr.length = arr.length - 1; 
+        return arrLast;
+    }
     
     
 }
-const aList = new Alist([1, 2, 3])
+const aList = new Alist([1, 2, 3, 4, 5])
 console.log(aList.addStart(123));
 console.log(aList.addEnd(123));
+console.log(aList.delStart());
+console.log(aList.delEnd());
