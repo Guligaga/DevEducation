0a853358a7d2588cc38a58a39d503bd10f13ca61
Evgeniy Koval <glggboko@gmail.com>
2020-02-22 10:30:01 +0200

    Replace functions to prototypes

--- src/aList/aList.js
+++ src/aList/aList.js
@@ -1,85 +1,155 @@
-
-function Alist(arr) {    
-    
-    this.addStart = function addStart(value) {
-        
-        let arrOut = [value];
-        for (let i = 0; i < arr.length; i++) {
-            arrOut[i + 1] = arr[i]
-        }
-        return arrOut;
-    }
-
-    this.addEnd = function addEnd(value) {
-
-        let arrOut = [];
-        for (let i = 0; i < arr.length; i++)
-        {
-            arrOut[i] = arr[i]
-        }
-        arrOut[arr.length] = value;
-        
-        return arrOut;
-    }
-
-    this.delStart = function delStart() {
-
-        let arrFirst = arr[0]
-        
-        for (let i = 0; i < (arr.length - 1); i++) {
-            
-            arr[i] = arr[i + 1]
-            
-        }
-        arr.length = arr.length - 1; 
-        return arrFirst;
-    }
-
-    this.delEnd = function delEnd() {
-        
-        let arrLast = arr[arr.length - 1]
-
-        arr.length = arr.length - 1; 
-        return arrLast;
-    }
-
-    this.delPos = function delPos(index) {
-
-        
-        if (arr[index] == undefined) {
-            console.log(`Error! Index ${index} is not defined.`);
-        }
-        else {
-            let arrSeized = arr[index];
-            for (let i = index; i < (arr.length - 1); i++) {
-            
-                arr[i] = arr[i + 1]
-                
-            }
-            arr.length = arr.length - 1;
-             
-            return arrSeized;
-        }
-        
-    }
-
-    this.get = function get(index) {
-        return arr[index]
-    }
-
-    this.set = function set (index, value) {
-        arr[index] = value;
-        return arr;
-    }
-    
-    
+function Alist(arr) {
+    this.defaultArr = arr;
+    this.arr = [];
+    this.init();
 }
-const aList = new Alist([11, 22, 33, 44, 55])
 
-console.log('.addStart impact:', aList.addStart(123));
-console.log('.addEnd impact:', aList.addEnd(123));
-console.log('.delStart impact:', aList.delStart());
-console.log('.delEnd impact:', aList.delEnd());
-console.log('.delPos impact:', aList.delPos(4));
-console.log('.get impact:', aList.get(1));
-console.log('.set impact:', aList.set(1, 88));
+Alist.prototype.init = function() {
+    
+    let i = 0; 
+    while(this.defaultArr[i] !== undefined) {
+
+        this.arr[i] = this.defaultArr[i];
+        i++;
+    }
+    return this.arr;    
+}
+
+Alist.prototype.size = function() {
+    let i = 0;
+
+    while (this.arr[i] !== undefined) {
+        i++;
+    }
+    return i;
+}
+
+Alist.prototype.addStart = function(value) {
+
+    let arrOut = [value];
+    for (let i = 0; i < this.size(); i++) {
+        
+        arrOut[i + 1] = this.arr[i];
+    }
+    this.arr = arrOut;
+    return this.arr;
+}
+
+Alist.prototype.addEnd = function(value) {
+    
+    let arrOut = this.arr;
+    
+    arrOut[this.size()] = value;
+    this.arr = arrOut;
+    return this.arr;    
+}
+
+Alist.prototype.delStart = function() {
+    
+    let arrTemp = []
+    let arrFirst = this.arr[0]
+
+    for (let i = 0; i < (this.size() - 1); i++) {
+        
+        arrTemp[i] = this.arr[i + 1]
+        
+    }
+    this.arr = arrTemp;
+    console.log(this.arr)
+    return arrFirst;
+}
+
+Alist.prototype.delEnd = function() {
+    
+    let arrLast = this.arr[this.size() - 1]
+    let arrTemp = [];
+    for (let i = 0; i < (this.size() - 1); i++) {
+        arrTemp[i] = this.arr[i];
+    }
+    this.arr = arrTemp;
+    return arrLast;
+}
+
+Alist.prototype.delPos = function(index) {
+    
+    if (this.arr[index] == undefined) {
+        return `Error! Index ${index} is not defined.`;
+    }
+    else {
+        let arrSeized = this.arr[index];
+        let arrTemp = [];
+
+        for (let i = 0; i < index; i++) {
+            arrTemp[i] = this.arr[i];
+        }
+
+        for (let i = index; i < (this.size() - 1); i++) {
+            arrTemp[i] = this.arr[i + 1];
+        }
+        this.arr = arrTemp;
+        return arrSeized;
+    }    
+}
+
+Alist.prototype.get = function(index) {
+    
+    if (this.arr[index] == undefined) {
+        return `Error! Index ${index} is not defined.`;
+    }
+    else {
+        return this.arr[index];
+    }
+}
+
+Alist.prototype.set = function(index, value) {
+
+    if (this.arr[index] == undefined) {
+        return `Error! Index ${index} is not defined.`;
+    }
+    else {
+        this.arr[index] = value;
+        return this.arr;
+    }
+}
+
+Alist.prototype.toString = function() {
+    let j = 0;
+    arrTemp = '';
+    for (let i = 0; i < this.size(); i++) {
+
+        this.arr[i] = this.arr[i] + '';
+        arrTemp += this.arr[i];
+    }
+    this.arr = arrTemp;
+    return this.arr;
+}
+
+Alist.prototype.clear = function() {
+    
+    this.arr = this.defaultArr;
+    return this.arr;
+}
+
+Alist.prototype.min = function() {
+    
+    let arrMin = this.arr[0];
+    for (let i = 0; i < this.size(); i++) {
+        if (arrMin > this.arr[i]) {
+            arrMin = this.arr[i]
+        }
+    }
+    return arrMin;
+}
+
+Alist.prototype.max = function() {
+    
+    let arrMax = this.arr[0];
+    for (let i = 0; i < this.size(); i++) {
+        if (arrMax < this.arr[i]) {
+            arrMax = this.arr[i]
+        }
+    }
+    return arrMax;
+}
+const aList = new Alist([11, 22, 33, 44, 55]);
+
