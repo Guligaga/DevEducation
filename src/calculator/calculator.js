83d0187c96c4f4e2baa98059bdca7008cfa86cf8
Evgeniy Koval <glggboko@gmail.com>
2020-02-24 16:33:01 +0200

    Fix bugs with serial operations

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -3,6 +3,8 @@ let operand1 = '';
 let operand2 = '';
 let operator;
 let isOperClicked = false;
+let answer;
+let isNumClicked = false;
 const displayVar = document.querySelector(".display");
 const numeralVar = document.querySelectorAll('.numeral');
 const operatorVar = document.querySelectorAll(".operator");
@@ -14,23 +16,25 @@ for (let i = 0; i < numeralVar.length; i++) {
     numeralVar[i].addEventListener("click", showOnDisp);
 }
 function showOnDisp(event) {
-        
+    // debugger
     if (isOperClicked == false) {
 
         if (operand1.length < 10) {
             
             operand1 += event.target.value;
             displayVar.value = operand1;
+            
         }
     }
-    else if (isOperClicked == true) {
+    else {
         
         if (operand2.length < 10) {
-            
+
             operand2 += event.target.value;
             displayVar.value = operand2;
         }
     }
+    isNumClicked = true;
 }
 
 
@@ -40,8 +44,20 @@ for (let i = 0; i < operatorVar.length; i++) {
 }
 function operatorAction(event) {
 
+    if (isNumClicked == false) {
+    
+        operand1 = +answer;
+        operand2 = '';
+    }
+    if (operand1 !== '' && operand1 !== '' && isOperClicked == true) {
+
+        displayVar.value = getAnswer(operator);
+        operand1 = +answer;
+        operand2 = '';
+    }
     operator = event.target.value;
     isOperClicked = !!operator;
+    isNumClicked = false;
 }
 
 
@@ -49,31 +65,31 @@ equalityVar.addEventListener("click", equalityAction);
 
 function equalityAction() {
 
-    
-    // isOperClicked = false;
-
     if (operand1 !== '' && operand2 !== '') {
         
-        operand1 = +operand1;
-        operand2 = +operand2;
         displayVar.value = getAnswer(operator);
         
         isOperClicked = false;
+        isNumClicked = false;
         operand1 = '';
         operand2 = '';
+
+        
         
     }
     else if (operand1 !== '' && operand2 == ''){
 
         if (isOperClicked == true) {
             
-            operand1 = +operand1;
             operand2 = operand1;
             displayVar.value = getAnswer(operator);
-        
+            
             isOperClicked = false;
+            isNumClicked = false;
             operand1 = '';
             operand2 = '';
+
+            
         }
         else {
             displayVar.value = operand1;
@@ -83,12 +99,13 @@ function equalityAction() {
     else {
         if (isOperClicked == true) {
             operand1 = 0;
-            operand2 = + operand2;
             displayVar.value = getAnswer(operator);
         
             isOperClicked = false;
+            isNumClicked = false;
             operand1 = '';
             operand2 = '';
+
         }
         else {
             displayVar.value = 0;
@@ -98,9 +115,11 @@ function equalityAction() {
 
 }
 
-
 function getAnswer (operation) {
-    let answer;
+    
+    operand1 = +operand1;
+    operand2 = +operand2;
+
     switch (operation) {
         case '+':
             answer = calcSum(operand1, operand2);
@@ -116,7 +135,9 @@ function getAnswer (operation) {
             break;
         
     }
+    console.log(answer);
     return answer;
+
 }
 
 function calcSum(a, b) {
